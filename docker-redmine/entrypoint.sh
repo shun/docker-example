ab3f0360755b4ad4c696aa5efc9cc10a9433a77a
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2021-03-03 01:33:35 +0000

    add official redmine

---
+++ docker-redmine/entrypoint.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+set -eu
+
+## DATABASE
+DB_ADAPTER=${DB_ADAPTER:-}
+DB_ENCODING=${DB_ENCODING:-}
+DB_CHARSET=${DB_CHARSET:-}
+DB_HOST=${DB_HOST:-}
+DB_PORT=${DB_PORT:-}
+DB_NAME=${DB_NAME:-}
+DB_USER=${DB_USER:-}
+DB_PASS=${DB_PASS:-}
+DB_POOL=${DB_POOL:-5}
+
+## MAIL DELIVERY
+SMTP_METHOD=${SMTP_METHOD:-smtp}
+SMTP_DOMAIN=${SMTP_DOMAIN:-www.gmail.com}
+SMTP_HOST=${SMTP_HOST:-smtp.gmail.com}
+SMTP_PORT=${SMTP_PORT:-587}
+SMTP_USER=${SMTP_USER:-}
+SMTP_PASS=${SMTP_PASS:-}
+SMTP_OPENSSL_VERIFY_MODE=${SMTP_OPENSSL_VERIFY_MODE:-}
+SMTP_STARTTLS=${SMTP_STARTTLS:-true}
+SMTP_TLS=${SMTP_TLS:-false}
+SMTP_CA_ENABLED=${SMTP_CA_ENABLED:-false}
+SMTP_CA_PATH=${SMTP_CA_PATH:-$REDMINE_ROOT_PATH/certs}
+SMTP_CA_FILE=${SMTP_CA_FILE:-$REDMINE_ROOT_PATH/certs/ca.crt}
+SMTP_ENABLED=${SMTP_ENABLED:-true}
+SMTP_AUTHENTICATION=${SMTP_AUTHENTICATION:-:login}
+SMTP_ENABLED=${SMTP_ENABLED:-false}
+
+init_config() {
+  cp /assets/redmine/config/database.yml.org $REDMINE_ROOT_PATH/config/database.yml
+  cp /assets/redmine/config/configuration.yml.org $REDMINE_ROOT_PATH/config/configuration.yml
+}
+
+setup_configuration() {
+  local fpath=$REDMINE_ROOT_PATH/config/configuration.yml
+
+  sed -ri "s/[{]{2} ?SMTP_METHOD ?[}]{2}/$SMTP_METHOD/g" $fpath
+  sed -ri "s/[{]{2} ?SMTP_STARTTLS ?[}]{2}/$SMTP_STARTTLS/g" $fpath
+  sed -ri "s/[{]{2} ?SMTP_HOST ?[}]{2}/$SMTP_HOST/g" $fpath
+  sed -ri "s/[{]{2} ?SMTP_PORT ?[}]{2}/$SMTP_PORT/g" $fpath
+  sed -ri "s/[{]{2} ?SMTP_DOMAIN ?[}]{2}/$SMTP_DOMAIN/g" $fpath
+  sed -ri "s/[{]{2} ?SMTP_AUTHENTICATION ?[}]{2}/$SMTP_AUTHENTICATION/g" $fpath
+  sed -ri "s/[{]{2} ?SMTP_USER ?[}]{2}/$SMTP_USER/g" $fpath
+  sed -ri "s/[{]{2} ?SMTP_PASS ?[}]{2}/$SMTP_PASS/g" $fpath
+  sed -ri "s/[{]{2} ?SMTP_OPENSSL_VERIFY_MODE ?[}]{2}/$SMTP_OPENSSL_VERIFY_MODE/g" $fpath
+  sed -ri "s%[{]{2} ?SMTP_CA_PATH ?[}]{2}%$SMTP_CA_PATH%g" $fpath
+  sed -ri "s%[{]{2} ?SMTP_CA_FILE ?[}]{2}%$SMTP_CA_FILE%g" $fpath
+}
+
+setup_database() {
+  local fpath=$REDMINE_ROOT_PATH/config/database.yml
+
+  sed -ri "s/[{]{2} ?DB_ADAPTER ?[}]{2}/$DB_ADAPTER/g" $fpath
+  sed -ri "s/[{]{2} ?DB_ENCODING ?[}]{2}/$DB_ENCODING/g" $fpath
+  sed -ri "s/[{]{2} ?DB_CHARSET ?[}]{2}/$DB_CHARSET/g" $fpath
+  sed -ri "s/[{]{2} ?DB_HOST ?[}]{2}/$DB_HOST/g" $fpath
+  sed -ri "s/[{]{2} ?DB_PORT ?[}]{2}/$DB_PORT/g" $fpath
+  sed -ri "s/[{]{2} ?DB_NAME ?[}]{2}/$DB_NAME/g" $fpath
+  sed -ri "s/[{]{2} ?DB_USER ?[}]{2}/$DB_USER/g" $fpath
+  sed -ri "s/[{]{2} ?DB_PASS ?[}]{2}/$DB_PASS/g" $fpath
+  sed -ri "s/[{]{2} ?DB_POOL ?[}]{2}/$DB_POOL/g" $fpath
+}
+
+wait_db_mysql() {
+  echo "connect mysql >>"
+  until mysqladmin ping -h $DB_HOST -P $DB_PORT --silent;
+  do
+    echo "Waiting for database connection..."
+    sleep 5
+  done
+}
+
+wait_db_postgres() {
+  echo "connect postgresql >>"
+  until PGPASSWORD=$DB_PASS psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c '\q';
+  do
+    echo "Waiting for database connection..."
+    sleep 5
+  done
+}
+
+wait_db() {
+  if [ "${DB_ADAPTER}" = "mysql" ]; then
+      wait_db_mysql
+  else
+      wait_db_postgres
+  fi
+
+  echo "!! database is ready."
+}
+
+init_config
+setup_database
+setup_configuration
+
+wait_db
+
+if [ ! -e /init_done ]; then
+  sudo -HEu $REDMINE_USER bundle exec rake generate_secret_token
+  RAILS_ENV=production sudo -HEu $REDMINE_USER bundle exec rake db:migrate
+  touch /init_done
+  echo "##############################"
+  echo "#"
+  echo "# initial migration done."
+  echo "#"
+  echo "##############################"
+fi
+
+exec "$@"
+
